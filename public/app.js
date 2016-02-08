4021a4454ea7c85b8da566afe9e5b08daa12103c
ZeqingZhang <terryzhang1995@yahoo.com>
2016-02-07 23:50:00 -0500

    Template

--- public/app.js
+++ public/app.js
@@ -5,6 +5,30 @@ $(document).ready(function() {
     console.log("document ready");
 
 
+    $.Slitslider.defaults 	= {
+        // transitions speed
+        speed : 800,
+        // if true the item's slices will also animate the opacity value
+        optOpacity : false,
+        // amount (%) to translate both slices - adjust as necessary
+        translateFactor : 230,
+        // maximum possible angle
+        maxAngle : 25,
+        // maximum possible scale
+        maxScale : 2,
+        // slideshow on / off
+        autoplay : true,
+        // keyboard navigation
+        keyboard : true,
+        // time between transitions
+        interval : 4000,
+        // callbacks
+        onBeforeChange : function( slide, idx ) { return false; },
+        onAfterChange : function( slide, idx ) { return false; }
+    };
+
+
+
     $('#button01').click(function () {
         console.log("click button01");
         $('#Unity').modal({
@@ -41,7 +65,7 @@ $(document).ready(function() {
 
     $('#button04').click(function () {
         console.log("click button04");
-        $('#ajax').modal({
+        $('#StateStreet').modal({
             show: true
         });
     });
