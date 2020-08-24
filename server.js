b7c836541d9014a7583bfa69c9d2c9c3b82610a0
Zeqing Zhang <terry.zhang@cspi.com>
2020-08-23 22:01:55 -0400

    Update

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./public/server/service/contactService.js')(app);
 
-aapp.use(express.static(__dirname + "/dist/personalWebpage"));
+app.use(express.static(__dirname + "/dist/personalWebpage"));
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/personalWebpage/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/personalWebpage/public/index.html"));
 });
+// app.use(express.static(__dirname + '/public'));
 
 
 app.listen(process.env.PORT || 8080);
